2e31712fde6f45ab96f13e3c0311446438069cc2
Nardis <nardis9501@gmail.com>
2023-12-30 17:55:49 -0500

    feat: add react router dom

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { RouterProvider, createHashRouter } from "react-router-dom";
+const router = createHashRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
