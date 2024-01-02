554651be473339f3dbe6715ee474be305c22286a
Nardis <nardis9501@gmail.com>
2024-01-01 21:27:42 -0500

    feat: change navbar icons

--- src/components/Navbar/Navbar.data.jsx
+++ src/components/Navbar/Navbar.data.jsx
@@ -1,14 +1,16 @@
-import AboutIcon from "../../utils/icons/AboutIcon";
-import HomeIcon from "../../utils/icons/HomeIcon";
-import ProjectIcon from "../../utils/icons/ProjectIcon";
-import SkillIcon from "../../utils/icons/SkillIcon";
-import WorkIcon from "../../utils/icons/WorkIcon";
+import {
+  RiAccountCircleFill,
+  RiBriefcase4Fill,
+  RiCodeSSlashFill,
+  RiHome3Line,
+  RiListCheck3,
+} from "react-icons/ri";
 
 export const dataNavbar = [
-  { name: "home", path: "/", icon: <HomeIcon /> },
-  { name: "about", path: "/about", icon: <AboutIcon /> },
-  { name: "works", path: "/works", icon: <WorkIcon /> },
-  { name: "projects", path: "/projects", icon: <ProjectIcon /> },
+  { name: "home", path: "/", icon: <RiHome3Line size={30} /> },
+  { name: "about", path: "/about", icon: <RiAccountCircleFill size={30} /> },
+  { name: "works", path: "/works", icon: <RiBriefcase4Fill size={30} /> },
+  { name: "projects", path: "/projects", icon: <RiListCheck3 size={30} /> },
 
-  { name: "skills", path: "/skills", icon: <SkillIcon /> },
+  { name: "skills", path: "/skills", icon: <RiCodeSSlashFill size={30} /> },
 ];
